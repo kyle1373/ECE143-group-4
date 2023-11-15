33b873f32d721e5d9b1d6ce449c346e0af271b81
Kyle Wade <59634395+kyle1373@users.noreply.github.com>
2023-11-15 03:26:40 -0800

    Added data and visualizations with correlation matrix + segmented user groups

---
+++ visualizations/app.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_path_csv = '../data/data.csv'
+data = pd.read_csv(file_path_csv)
+
+data.head()
+
+# Devices Used
+plt.figure(figsize=(10, 6))
+sns.countplot(x='DEVICE_TYPE_W63', data=data)
+plt.title('Distribution of Devices Used')
+plt.ylabel('Count')
+plt.xlabel('Type of Device')
+plt.xticks(ticks=[0, 1, 2], labels=['Desktop/Laptop', 'Smartphone', 'Tablet'])
+plt.show()
+
+# Age Category
+plt.figure(figsize=(10, 6))
+sns.countplot(x='F_AGECAT', data=data)
+plt.title('Distribution of Respondents by Age')
+plt.ylabel('Count')
+plt.xlabel('Age Category')
+plt.show()
+
+# Education Level
+plt.figure(figsize=(10, 6))
+sns.countplot(x='F_EDUCCAT', data=data)
+plt.title('Distribution of Respondents by Level of Education')
+plt.ylabel('Count')
+plt.xlabel('Level of Education')
+plt.show()
+
+# Correlation Matrix
+subset_columns = [
+    'DEVICE_TYPE_W63',
+    'LANG_W63_W63',
+    'SNSUSE_W63',
+    'VIDEOGAME_W63',
+    'F_AGECAT',
+    'F_EDUCCAT',
+    'F_INCOME',
+    'SMARTPHONE_W63',
+    'F_SEX'
+]
+
+correlation_data = data[subset_columns]
+correlation_matrix = correlation_data.corr()
+
+plt.figure(figsize=(12, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+plt.title('Correlation Matrix')
+plt.show()
